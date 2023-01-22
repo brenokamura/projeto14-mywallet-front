9767b78833f50a343cc2758bfed514dd12f17d0a
brenokamura <96897837+brenokamura@users.noreply.github.com>
2023-01-22 04:44:43 -0300

    Exit implementado

---
+++ src/Pages/Exit.js
@@ -0,0 +1,67 @@
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import UserContext from '../../contexts/UserContext';
+
+function Saida() {
+  const URL = http://localhost:5009/transacoes
+  const { user } = useContext(UserContext);
+  const [value, setValue] = useState('');
+  const [description, setDescription] = useState('');
+  const navigator = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const body = {
+      description,
+      type: 'saida',
+      value: parseFloat(value)
+    };
+    const headers = {
+      headers: { Authorization: `Bearer ${user.token}` }
+    };
+    try {
+      await axios.post(URL, body, headers);
+      alert('Registro feito com sucesso!');
+      navigator('/home');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <Title color={colors.text}>Nova saída</Title>
+      <form>
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+       <input
+          type="text"
+          placeholder="Descrição"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+
+        <button type="submit" onClick={handleSubmit}>
+          Salvar entrada
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Saida;
+
+const Title = styled.h1`
+  font-family: "Raleway", normal;
+  font-size: 26px;
+  color: ${(props) => props.color};
+  width: 168px;
+  height: 31px;
+  left: 24px;
+  top: 25px;
+`;
